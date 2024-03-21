84d028b2fdc6709b987f8f15be22baa1a99209d0
Brian Petro-Roy <twxabfn+github@gmail.com>
2024-03-21 12:32:07 -0400

    Added product database and page template

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,5 +1,11 @@
+from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, "myapp/index.html")
+
+def products(request):
+    context = {"products_list": Product.objects.all()}
+    return render(request, "myapp/products.html", context)
